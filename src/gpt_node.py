4ae390267faf994f5e07af7503f1a81151a787dc
jeromekirchhoff <100201541+jeromekirchhoff@users.noreply.github.com>
2024-04-02 11:48:24 +0200

    Added license information

--- src/gpt_node.py
+++ src/gpt_node.py
@@ -1,4 +1,12 @@
 #!/path to python interpreter
+
+# Copyright (c) 2024 Fachgebiet Simulation, Systemoptimierung und Robotik, TU Darmstadt.
+
+# This file is part of Co-Speech Gesture Pipeline
+# (see https://github.com/tu-darmstadt-ros-pkg/co-speech_gesture_pipeline) 
+# and is governed by a BSD-style license 
+# that can be found in the LICENSE file.
+
 import json
 import time
 import openai
